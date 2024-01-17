1ac98cb55191d388afe1be2dd9ed982350bbde6c
DanielChistovaoDev <contato.danielchistovao@gmail.com>
2024-01-17 05:59:25 -0300

    feat: integracao pagina home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,41 +1,27 @@
 // src/pages/Home.tsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.css';
 import DragonTable from '../../components/DragonTable/DragonTable';
+import useDragons from '../../hooks/useDragons';
 
 const Home: React.FC = () => {
+  const { dragons, loading, error, getDragons } = useDragons();
 
-  const dragonsMock = [
-    {
-      "createdAt":"2024-01-16T18:43:11.561Z",
-      "name":"Iabaasdadad",
-      "type":"Ice",
-      "histories":["adasdasda"],
-      "id":"1"
-    },
-    {
-      "createdAt":"2024-01-16T18:43:11.561Z",
-      "name":"asdadad",
-      "type":"Ice",
-      "histories":["adasdasda"],
-      "id":"2"
-    },
-    {
-      "createdAt":"2024-01-16T18:43:16.694Z",
-      "name":"wqeqeqe",
-      "type":"Water",
-      "histories":["asdasdada"],
-      "id":"3"
-    }
-  ]
- 
+  useEffect(() => {
+    getDragons();
+  }, []);
+
   return (
     <div className="home-container">
       <h2>Página Inicial</h2>
       <p>Bem-vindo à sua página inicial!</p>
-      <DragonTable
-        dragons={dragonsMock}
-      />
+      {loading ? (
+        <p>Carregando dragões...</p>
+      ) : error ? (
+        <p>Erro ao carregar dragões: {error.message}</p>
+      ) : (
+        <DragonTable dragons={dragons} />
+      )}
     </div>
   );
 };
