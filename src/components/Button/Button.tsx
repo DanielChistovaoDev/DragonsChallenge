dd10776d3a76373361b4f72416f8250268f55527
DanielChistovaoDev <contato.danielchistovao@gmail.com>
2024-01-18 11:52:52 -0300

    feat: componente de botao

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Button.css';
+
+interface ButtonProps {
+  onClick: () => void;
+  label: string;
+  disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+    onClick,
+    label,
+    disabled
+}) => {
+  return (
+    <button
+        className='button'
+        onClick={onClick}
+        disabled={disabled}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
