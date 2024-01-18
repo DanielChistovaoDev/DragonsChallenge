5d2a58332d78b6c782a32a11b0f06b3379661716
DanielChistovaoDev <contato.danielchistovao@gmail.com>
2024-01-18 04:37:42 -0300

    feat: background login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,3 @@
-// src/components/Login.tsx
 import React, { useState } from 'react';
 import './Login.css';
 import useAuth from '../../hooks/useAuth';
@@ -30,29 +29,31 @@ const Login: React.FC = () => {
 
 
   return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <input
-        type="text"
-        placeholder="Username"
-        className="login-input"
-        value={credentials.username}
-        onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        className="login-input"
-        value={credentials.password}
-        onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
-      />
-      <button className="login-button" onClick={handleLoginClick}>
-        Login
-      </button>
-      <button className="login-button" onClick={handleLogoutClick}>
-        Logout
-      </button>
-      <p>Is logged in: {isLoggedIn ? 'Yes' : 'No'}</p>
+    <div className="login-background-container">
+      <div className="login-container-wrapper">
+        <h2 className="login-title">Login</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          className="login-input"
+          value={credentials.username}
+          onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="login-input"
+          value={credentials.password}
+          onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+        />
+        <button className="login-button" onClick={handleLoginClick}>
+          Login
+        </button>
+        <button className="login-button" onClick={handleLogoutClick}>
+          Logout
+        </button>
+        <p>Is logged in: {'No'}</p>
+      </div>
     </div>
   );
 };
